853033a9a782b998c630bee155ac81656b04de52
Alberto Barnabo <alberto.barnabo@gmail.com>
2026-04-27 23:16:50 +0200

    table background

--- src/components/JobsTable.tsx
+++ src/components/JobsTable.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useMemo } from "react";
 import { JobEntry } from "../../lib/types";
-import background from "../../public/background.png";
+import tableBackground from "../../public/table_background.png";
 
 interface JobsTableProps {
   jobs: JobEntry[];
@@ -71,7 +71,7 @@ export default function JobsTable({ jobs, onEditJob }: JobsTableProps) {
       <div
         className="absolute inset-0 bg-cover bg-center"
         style={{
-          backgroundImage: `url(${background.src})`,
+          backgroundImage: `url(${tableBackground.src})`,
         }}
       />
 
